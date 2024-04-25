a9a85fd667d6e7155ca8cf4a25d36ded0a6f887e
alinenunes21 <alinenunes21@hotmail.com>
2024-04-25 11:47:05 -0300

    exercicio dos vetores

--- OneDrive/Área de Trabalho/ConstruçaoDeSoftware/ConstrucaoDeSoftware-main/atividade03/test/java/Bairro.java
+++ OneDrive/Área de Trabalho/ConstruçaoDeSoftware/ConstrucaoDeSoftware-main/atividade03/test/java/Bairro.java
@@ -1,7 +1,7 @@
 public class Bairro {
 
         private String nome;
-        public void adicionarEndereco(Endereco endereco)
+        public void adicionarEndereco(Endereco endereco);
         public void setCidade(Cidade cidade) {
 
         }
